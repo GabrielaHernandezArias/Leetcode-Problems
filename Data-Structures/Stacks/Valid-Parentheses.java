5ef836befd151f227762955ddb5c430e2308df43
Gabriela Hernandez <gabriela.hernandez78018@gmail.com>
2021-12-20 11:58:11 -0500

    Update Valid-Parentheses.java

--- Data-Structures/Stacks/Valid-Parentheses.java
+++ Data-Structures/Stacks/Valid-Parentheses.java
@@ -23,7 +23,7 @@ public boolean isValid(String s) {
     
 	Stack<Character> stack = new Stack<Character>();
 	
-    for (char c : s.toCharArray()) {
+	for (char c : s.toCharArray()) {
 		if (c == '(')
 			stack.push(')');
 		else if (c == '{')
